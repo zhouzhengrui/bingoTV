ab8f5f6730697b92bf83a4ed16f5793333ce4535
周正锐 <zhouzhengrui@gmail.com>
2019-01-18 15:20:23 +0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,125 @@
+// iScroll
+function isPassive() {
+    var supportsPassiveOption = false;
+    try {
+        addEventListener("test", null, Object.defineProperty({}, 'passive', {
+            get: function() {
+                supportsPassiveOption = true;
+            }
+        }));
+    } catch (e) {}
+    return supportsPassiveOption;
+}
+
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, isPassive() ? {
+    capture: false,
+    passive: false
+} : false);
+
+var mainScroll;
+
+function loaded() {
+
+    mainScroll = new IScroll('.main', {
+        scrollbars: 'custom',
+        fadeScrollbars: true,
+        shrinkScrollbars: 'scale',
+        preventDefaultException: {tagName: /^(INPUT|TEXTAREA|BUTTON|SELECT|IMG)$/ },
+        probeType: 2,
+        tap: true
+    });
+
+}
+
+// swiper
+var imageSlide = new Swiper('#image-slide', {
+    loop: true,
+    autoplay: 5000,
+    lazyLoading: true,
+    lazyLoadingInPrevNext: true,
+    pagination: '.swiper-pagination'
+});
+
+// waves
+Waves.displayEffect();
+
+// delayed spik
+function delayedSpik() {
+
+    var href = [];
+    $("[data-href]").each(function(i) {
+        href[i] = $(this).data('href');
+        if ($(this).hasClass('waves-effect')) {
+            $(this).bind("tap", function() {
+                window.setTimeout(function() {
+                    location.href = href[i];
+                }, 300);
+            });
+        } else {
+            $(this).bind("tap", function() {
+                location.href = href[i];
+            });
+        }
+    });
+
+    $("footer [data-href]").each(function(i) {
+        href[i] = $(this).data('href');
+        if ($(this).hasClass('waves-effect')) {
+            $(this).bind("click", function() {
+                window.setTimeout(function() {
+                    location.href = href[i];
+                }, 300);
+            });
+        } else {
+            $(this).bind("click", function() {
+                location.href = href[i];
+            });
+        }
+    });
+
+};
+delayedSpik();
+
+// toast
+function toast(message, duration, position, align) {
+    if (typeof(position) != "undefined" && position != "") {
+        position = "toast-" + position;
+    } else {
+        position = "";
+    }
+    if (typeof(align) != "undefined" && align != "") {
+        align = "align-" + align;
+    } else {
+        align = "";
+    }
+    duration = duration || 3000;
+    duration = isNaN(duration) ? 3000 : duration;
+    var m = document.createElement('div');
+    m.setAttribute("class", "toast " + position + " " + align);
+    m.innerHTML = message;
+    document.body.appendChild(m);
+    setTimeout(function() {
+        m.setAttribute("class", "toast show " + position + " " + align);
+        setTimeout(function() {
+            m.setAttribute("class", "toast  " + position + " " + align);
+            setTimeout(function() {
+                document.body.removeChild(m);
+            }, 300);
+        }, duration);
+    }, 100);
+}
+
+// jQuery
+jQuery(document).ready(function($) {
+
+    // button
+    $('.button').on('tap', function() {
+        $(this).removeClass('active').addClass('active');
+        var set = setTimeout(function() {
+            $('.button').removeClass('active');
+        }, 100)
+    });
+
+});
